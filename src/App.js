95bb02ccd3362da758748fe9949dbe32bdeac4c7
Yasemin CUNİ <yasemin.cuni@TRHQMLCHQTKY.local>
2022-12-25 21:39:43 +0300

    project complated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Search from './components/Search';
+import AddApponitment from './components/AddApponitment';
+//import appointmentList from './data.json';
+import AppointmentInfo from './components/AppointmentInfo'
+import {useEffect, useState, useCallback} from 'react';
+import {BiCalendar} from 'react-icons/bi'
 function App() {
+  const [appointmentList, setAppointmentList] = useState([]);
+  const [query, setQuery] = useState('');
+  const [sortBy, setSortBy] = useState("petName");
+  const [orderBy, setOrderBy] = useState('asc');
+  
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+      setAppointmentList(data)
+    })
+  },[]);
+
+  const filteredAppointments = appointmentList.filter((item) => {
+    return (
+      item.petName.toLowerCase().includes(query.toLowerCase()) ||
+      item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+      item.aptNotes.toLowerCase().includes(query.toLowerCase())
+    )
+  }).sort((a,b) => {
+    let order = (orderBy === 'asc') ? 1 : -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
+    )
+  })
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData])
+  
+
   return (
-    <div className="App">
-      <h1>Helloğğğ</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className='text-5xl mb-3'> <BiCalendar className='inline-block text-red-400 align-top'/> Your Appointment </h1>
+      <AddApponitment 
+        onSendAppointment={myAppointment => setAppointmentList([...appointmentList, myAppointment])}  
+        lastId={appointmentList.reduce((max,item) => Number(item.id) > max ? Number(item.id) : max, 0)}
+      />
+      <Search 
+        query={query} 
+        onQueryChange={myQuery => setQuery(myQuery)}
+        orderBy={orderBy}
+        onOrderByChange={myOrder => setOrderBy(myOrder)}
+        sortBy={sortBy}
+        onSortByChange={mySort => setSortBy(mySort)}
+      />
+      <ul className='divide-y divide-gray-200'>
+        {filteredAppointments
+          .map(appointment => (
+            <AppointmentInfo 
+              key={appointment.id} 
+              appointment={appointment}
+              onDeleteAppointment={
+                appoinmentId => setAppointmentList(appointmentList.filter(appointment => appointment.id !== appoinmentId))
+              }  
+            />
+          ))
+        }
+      </ul>
     </div>
   );
 }
---
+++ src/components/AddApponitment.js
@@ -0,0 +1,98 @@
+
+import {BiCalendarPlus} from 'react-icons/bi';
+import {useState} from 'react';
+const Appointment  = ({onSendAppointment, lastId}) => {
+  const clearData = {
+    ownerName: '',
+    petName: '',
+    aptDate: '',
+    aptTime: '',
+    aptNotes: ''
+  };
+  const [toggleForm, setToggleForm] = useState(false);
+  const [formData, setFormData] = useState(clearData);
+  const formDataPublish = () => {
+    const appointmentInfo = {
+      id: lastId + 1,
+      ownerName: formData.ownerName,
+      petName: formData.petName,
+      aptDate: formData.aptDate + ' ' + formData.aptTime,
+      aptNotes: formData.aptNotes 
+    }
+    onSendAppointment(appointmentInfo);
+    setFormData(clearData);
+    setToggleForm(!toggleForm);
+  }
+  return (
+    <div>
+      <button className={`bg-blue-400 text-white px-2 py-3 w-full text-left ${toggleForm ? 'rounded-t-md' : 'rounded-md'}`} onClick={() => setToggleForm(!toggleForm)}>
+        <div><BiCalendarPlus className="inline-block align-text-top" />  Add Appointment</div>
+      </button>
+      { toggleForm && (
+        <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="ownerName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Owner Name
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input type="text" name="ownerName" id="ownerName" value={formData.ownerName} onChange={(event)=>{setFormData({...formData, ownerName: event.target.value})}}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+            </div>
+          </div>
+  
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="petName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Pet Name
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input type="text" name="petName" id="petName" value={formData.petName} onChange={(event) => {setFormData({...formData, petName: event.target.value})}}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+            </div>
+          </div>
+  
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="aptDate" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Apt Date
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input type="date" name="aptDate" id="aptDate" value={formData.aptDate} onChange={(event) => {setFormData({...formData, aptDate: event.target.value})}}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+            </div>
+          </div>
+  
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="aptTime" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Apt Time
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input type="time" name="aptTime" id="aptTime" value={formData.aptTime} onChange={(event) => {setFormData({...formData, aptTime: event.target.value})}}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+            </div>
+          </div>
+  
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="aptNotes" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Appointment Notes
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <textarea id="aptNotes" name="aptNotes" rows="3" value={formData.aptData} onChange={(event) => {setFormData({...formData, aptNotes: event.target.value})}}
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Detailed comments about the condition"></textarea>
+            </div>
+          </div>
+  
+  
+          <div className="pt-5">
+            <div className="flex justify-end">
+              <button type="submit" onClick={formDataPublish} className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+    </div>
+  )
+}
+
+export default Appointment;
